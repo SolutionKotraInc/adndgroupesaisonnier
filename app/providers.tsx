b5796f91a72791192cbada795a910ad351944dbd
SolutionKotraInc <olivier.pilon@kotra.app>
2025-09-15 22:51:05 +0200

    Simplify providers to fix React types issue

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,37 +1,5 @@
 "use client";
 
-import React, { useEffect } from "react";
-
-export function SmoothScrollProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // Temporarily disable Lenis to fix webpack error
-    // TODO: Re-enable Lenis once webpack issue is resolved
-    /*
-    let lenis: any;
-    
-    const initLenis = async () => {
-      const Lenis = (await import("lenis")).default;
-      lenis = new Lenis({
-        lerp: 0.08,
-        smoothWheel: true
-      });
-
-      function raf(time: number) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-      requestAnimationFrame(raf);
-    };
-
-    initLenis();
-
-    return () => {
-      if (lenis) {
-        lenis.destroy?.();
-      }
-    };
-    */
-  }, []);
-
+export function SmoothScrollProvider({ children }: { children: any }) {
   return <>{children}</>;
 }
